5a0945bcedd0e479929dc0c842c6d8b84b84edc5
technotrend <technotrend@github.com>
2020-01-01 20:23:08 +0530

    second commit to social login

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,7 @@
 export const httpConstants = {
   SOCIAL_LOGIN: {
-    GOOGLE_OATH_CLIENT_ID:
-      "49804075883-69fr9hfpctf91a57s0t5vphbdn8kv2s9.apps.googleusercontent.com",
-    GOOGLE_SECRET: "OLBoUJqvRmxBiOlsixc37TxN",
-    FACEBOOK_APP_ID: "1736930459770353"
+    GOOGLE_OATH_CLIENT_ID: "Your oath Key",
+    GOOGLE_SECRET: "Your Google secret",
+    FACEBOOK_APP_ID: "Your facebook app id"
   }
 };
